05ce2d5140cd4924b78d8395ed3652e2a918458a
mdykiel <miro24@gmail.com>
2023-04-25 23:26:05 +0200

    Update Players.js

--- src/components/Players.js
+++ src/components/Players.js
@@ -4,7 +4,7 @@ import PlayerItem from './PlayerItem';
 const Players = (props) => (
     <div className="player">
         {
-            props.players.length === 0 ? "Użyj powyższego kodu aby dołączyć do gry."
+            props.players.length === 0 ? "Przekaż powyższy kod pokoju graczom aby dołączyli do gry."
                 : props.players.map((player) => {
                     return <PlayerItem key={player.name} name={player.name} score={player.score} stroke={player.stroke} colour={player.colour} />
                 })
