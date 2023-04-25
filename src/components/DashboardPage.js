881bfbeab171a0b7bf655563506d8fd572a412ed
mdykiel <miro24@gmail.com>
2023-04-25 22:59:07 +0200

    Update DashboardPage.js

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -21,15 +21,15 @@ export class DashboardPage extends React.Component {
             <div className="box-layout">
                 <Fade>
                     <div className="box-layout__box">
-                        <h1 className="box-layout__title">OpenTrivia</h1>
-                        <h3 className="box-layout__subtitle">A party trivia game in your web browser. <br></br> No downloading apps just create a game on the big screen and join up with your mobile device to play.</h3>
+                        <h1 className="box-layout__title">Afuek Quiz</h1>
+                        <h3 className="box-layout__subtitle">Wieloosobowy quiz wiedzy Audytu Finansowego. <br></br> Sprawdź swoją wiedzę grając z przyjaciółmi z Twojego roku</h3>
                         <div className="box-layout__button-container">
                             <div className="box-layout__button">
-                                <button className="button" onClick={this.startAsHost}>Create Game</button>
+                                <button className="button" onClick={this.startAsHost}>Utwórz Grę</button>
                             </div>
 
                             <div className="box-layout__button">
-                                <button className="button" onClick={this.startAsPlayer}>Join Game</button>
+                                <button className="button" onClick={this.startAsPlayer}>Dołącz do Gry</button>
                             </div>
 
                         </div>
